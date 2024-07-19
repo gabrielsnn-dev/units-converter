90191764ee62587640d7654b53bc5bf0ffa73556
Gabriel Senna <122230499+gabrielsnn-dev@users.noreply.github.com>
2024-07-18 22:17:36 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+
+# unit converter introduction
+print('Welcome to the unit converter!')
+
+option = -1
+
+while True:
+    try:
+        option = float(input('[1] Inches to Centimeters  [2] Yards to Meters   [0] Leave\n ->> '))
+
+        # stop the code if the user press 0
+        if option == 0:
+            break
+        
+        # inches to centimeters
+        if option == 1:
+            inch = float(input('Type the inches: '))
+            cm = inch * 2.54
+            print(f'{inch:.2f} inches is equal to {cm:.2f}cm')
+
+        # yards to meters
+        if option == 2:
+            yard = float(input('Type the yards: '))
+            m = yard / 1.094
+            print(f'{yard:.2f} yards is equal to {m:.2f}m')
+        
+    except ValueError:
+        print('That is not an option, try again.')
+
+    
+
+# functions to convert the units
